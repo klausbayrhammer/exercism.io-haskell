c98ab6505053ad4adf2cfc330e4f6c1e555ea1c7
Klaus Bayrhammer <klaus.bayrhammer@redbull.com>
2020-08-19 22:43:54 +0200

    feat(Pangram): fifth iteration

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -3,8 +3,6 @@ module Pangram (isPangram) where
 import Data.Char (toLower)
 
 isPangram :: String -> Bool
-isPangram text = isPangramRec ['a'..'z']  (map toLower text)
-
-isPangramRec :: [Char] -> String -> Bool
-isPangramRec [] _ = True
-isPangramRec (charToTest:remainingCharsToTest) text = elem charToTest text && isPangramRec remainingCharsToTest text
+isPangram text = all (\charToTest -> elem charToTest lowerCaseText) ['a'..'z']
+    where
+        lowerCaseText = map toLower text
