7b9a5a82d7d5335fa408783a4fc0eef75a798c26
Klaus Bayrhammer <klaus.bayrhammer@redbull.com>
2020-08-19 22:34:04 +0200

    feat(Pangram): Third iteration

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -1,22 +1,10 @@
 module Pangram (isPangram) where
 
-import Data.List (nub)
 import Data.Char (toLower)
 
 isPangram :: String -> Bool
-isPangram text = length (uniqueLowercaseCharsFromAToZ text) == length relevantLetters
+isPangram text = isPangramRec ['a'..'z']  (map toLower text)
 
-relevantLetters :: [Char]
-relevantLetters = ['a'..'z']
-
-uniqueLowercaseCharsFromAToZ :: String -> String
-uniqueLowercaseCharsFromAToZ text = nub (filterCharsFromAToZ (toLowerCase text))
-
-filterCharsFromAToZ :: String -> String
-filterCharsFromAToZ string = filter isNumberFromAToZ string
-
-isNumberFromAToZ :: Char -> Bool
-isNumberFromAToZ test = elem test relevantLetters
-
-toLowerCase :: String -> String
-toLowerCase mixedCaseString = map toLower mixedCaseString
+isPangramRec :: [Char] -> String -> Bool
+isPangramRec [] _ = True
+isPangramRec (x:xs) text = elem x text && isPangramRec xs text
