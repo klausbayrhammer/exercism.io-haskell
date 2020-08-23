98b6b44254df83eff786ca71f4cb8bf87b50c442
Klaus Bayrhammer <klaus.bayrhammer@redbull.com>
2020-08-23 19:22:09 +0200

    feat(Bob): third iteration

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -5,31 +5,16 @@ import Data.List (dropWhileEnd)
 
 responseFor :: String -> String
 responseFor xs
- | isSilence trimmedText = "Fine. Be that way!"
- | isForcefulQuestion trimmedText = "Calm down, I know what I'm doing!"
- | isQuestion trimmedText = "Sure."
- | isShouting trimmedText = "Whoa, chill out!"
+ | isSilence = "Fine. Be that way!"
+ | isForcefulQuestion = "Calm down, I know what I'm doing!"
+ | isQuestion = "Sure."
+ | isShouting = "Whoa, chill out!"
  | otherwise = "Whatever."
     where
-        trimmedText = trim xs
-
-isShouting :: String -> Bool
-isShouting x = isUpperCaseEqual x && (isLowerCaseNotEqual x)
-
-isUpperCaseEqual :: String -> Bool
-isUpperCaseEqual x = map toUpper x == x
-
-isLowerCaseNotEqual :: String -> Bool
-isLowerCaseNotEqual x = map toLower x /= x
-
-isQuestion :: String -> Bool
-isQuestion x = last x == '?'
-
-isSilence :: String -> Bool
-isSilence x = x == []
-
-isForcefulQuestion :: String -> Bool
-isForcefulQuestion x = isQuestion x && isShouting x
-
-trim :: String -> String
-trim xs = dropWhileEnd isSpace xs
+        trimmedText = dropWhileEnd isSpace xs
+        isSilence = trimmedText == []
+        isForcefulQuestion = isQuestion && isShouting
+        isQuestion = last trimmedText == '?'
+        isShouting = isUpperCaseEqual && isLowerCaseNotEqual
+        isUpperCaseEqual = map toUpper trimmedText == trimmedText
+        isLowerCaseNotEqual = map toLower trimmedText /= trimmedText
