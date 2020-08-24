654273f711c7a96bec23a3e9a1551e4eea7acfe2
Klaus Bayrhammer <klaus.bayrhammer@redbull.com>
2020-08-24 10:54:29 +0200

    feat(CollatzConjecture): third iteration

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -3,15 +3,15 @@ module CollatzConjecture (collatz) where
 collatz :: Integer -> Maybe Integer
 collatz initialNumber
     | isZeroOrNegative = Nothing
-    | otherwise = Just (collatzRec initialNumber 0)
+    | otherwise = Just (collatzRec initialNumber)
     where
         isZeroOrNegative = initialNumber <= 0
 
-collatzRec :: Integer -> Integer -> Integer
-collatzRec initialNumber numberOfSteps
-    | isOne = numberOfSteps
-    | isEven = collatzRec (div initialNumber 2) (numberOfSteps + 1)
-    | otherwise = collatzRec (initialNumber * 3 + 1) (numberOfSteps + 1)
+collatzRec :: Integer -> Integer
+collatzRec initialNumber
+    | isOne = 0
+    | isEven = 1 + collatzRec (div initialNumber 2)
+    | otherwise = 1 + collatzRec (initialNumber * 3 + 1)
     where
         isOne = initialNumber == 1
         isEven = rem initialNumber 2 == 0
