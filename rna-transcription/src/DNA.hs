9fbb44d8df23c8fcadc6d4d5ff7dee9e529c25f7
Klaus Bayrhammer <klaus.bayrhammer@redbull.com>
2020-08-30 22:11:45 +0200

    feat(DNA): third iteration

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -1,5 +1,4 @@
 module DNA (toRNA) where
-import Data.Either (partitionEithers)
 
 dnaRnaComplements :: Char -> Either Char Char
 dnaRnaComplements 'C' = Right 'G'
@@ -9,10 +8,5 @@ dnaRnaComplements 'A' = Right 'U'
 dnaRnaComplements x =  Left x
 
 toRNA :: String -> Either Char String
-toRNA xs
-    | hasErrors = Left (head bad)
-    | otherwise = Right good
-    where
-        (bad, good) = partitionEithers (map dnaRnaComplements xs)
-        hasErrors = length bad > 0
+toRNA xs = mapM dnaRnaComplements xs
 
