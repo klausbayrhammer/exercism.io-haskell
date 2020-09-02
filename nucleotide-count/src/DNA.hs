4a7453d38cc009a8abf017170bd0442b317ac702
Klaus Bayrhammer <klaus.bayrhammer@redbull.com>
2020-09-02 22:58:00 +0200

    feat(nucleotideCount): third iteration

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -1,9 +1,19 @@
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
-import Data.Map (Map, fromList)
+import Data.Map (Map, empty, insertWith)
 import Data.Either.Combinators  (mapRight)
+import Control.Monad (foldM)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
+
+nucleotideCounts :: String -> Either String (Map Nucleotide Int)
+nucleotideCounts xs = foldM aggregateNucelotides empty xs
+
+aggregateNucelotides :: Map Nucleotide Int -> Char -> Either String (Map Nucleotide Int)
+aggregateNucelotides acc dna = mapRight increaseNucleotideCount mappedDna
+    where
+        increaseNucleotideCount nucleotide = insertWith (+) nucleotide 1 acc
+        mappedDna = mapNucleotide dna
 
 mapNucleotide :: Char -> Either [Char] Nucleotide
 mapNucleotide 'A' = Right A
@@ -11,11 +21,3 @@ mapNucleotide 'C' = Right C
 mapNucleotide 'G' = Right G
 mapNucleotide 'T' = Right T
 mapNucleotide x = Left [x]
-
-countNucleotides :: [Nucleotide] -> Map Nucleotide Int
-countNucleotides xs = fromList (map countOccurences [A, C, G, T])
-    where
-        countOccurences nucleotide = (nucleotide, length (filter (== nucleotide) xs))
-
-nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = mapRight countNucleotides (mapM mapNucleotide xs)
