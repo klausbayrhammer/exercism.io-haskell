3edeb09c703aa1850890ac191a207589299b436e
Klaus Bayrhammer <klaus.bayrhammer@redbull.com>
2020-09-03 21:57:02 +0200

    feat(hamming): second iteration

--- hamming/src/Hamming.hs
+++ hamming/src/Hamming.hs
@@ -3,7 +3,7 @@ module Hamming (distance) where
 distance :: String -> String -> Maybe Int
 distance xs ys
     | areStrandsNotEqual = Nothing
-    | otherwise = Just (length (filter isTupleNotEqual zippedStrands))
+    | otherwise = Just $ length $ filter isTupleNotEqual zippedStrands
     where
         zippedStrands = zip xs ys
         isTupleNotEqual (a, b) = a /= b
