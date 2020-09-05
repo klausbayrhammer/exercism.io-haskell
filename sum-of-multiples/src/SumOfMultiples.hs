c16f57dae94325456777cd7cbde2a1607aee0bc2
Klaus Bayrhammer <klaus.bayrhammer@redbull.com>
2020-09-05 21:38:59 +0200

    feat(sumOfMultiples): second iteration

--- sum-of-multiples/src/SumOfMultiples.hs
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -1,8 +1,11 @@
 module SumOfMultiples (sumOfMultiples) where
 
+import Data.Set (fromList)
+
 sumOfMultiples :: [Integer] -> Integer -> Integer
 sumOfMultiples factors limit = sum multiples
     where
-        multiples = filter isMultipleOfFactors [1..limit-1]
-        isMultipleOfFactors potentialMultiple = any (\factor -> rem potentialMultiple factor == 0) factorsWithoutZero
+        multiples = fromList factorsUntilLimit
+        factorsUntilLimit = concat $ map multiplesOfFactorUntilLimit factorsWithoutZero
+        multiplesOfFactorUntilLimit factor = takeWhile (< limit) [factor, 2 * factor ..]
         factorsWithoutZero = filter (>0) factors
