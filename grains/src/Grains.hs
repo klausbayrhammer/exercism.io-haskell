c4408b9ed130fb46fb8d470fccf64e2b0eca85fa
kbayrhammer <klaus.bayrhammer@redbull.com>
2020-09-07 16:34:51 +0200

    feat(Grains): third iteration

--- grains/src/Grains.hs
+++ grains/src/Grains.hs
@@ -2,10 +2,11 @@ module Grains (square, total) where
 
 square :: Integer -> Maybe Integer
 square n
-    | isValidChessBoardPosition = Just (iterate (*2) 1 !! (fromIntegral n - 1))
+    | isValidChessBoardPosition = Just (grainsOnSquare)
     | otherwise = Nothing
     where
         isValidChessBoardPosition = n > 0 && n < 65
+        grainsOnSquare = iterate (*2) 1 !! (fromIntegral n - 1)
 
 total :: Integer
 total = 18446744073709551615
